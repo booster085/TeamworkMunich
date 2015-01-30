60986183a1b788b7502232e82f5017ff44b220b1
unknown <kirill2ll@mail.ru>
2015-01-30 22:55:53 +0200

    adding comments in main class

--- src/Bulls.java
+++ src/Bulls.java
@@ -3,6 +3,7 @@ public class Bulls {
 
 	public static int countBulls(int[] guessNum, int[] compNum){
 		int bulls=0;
+		// counting bulls - same digits of number 1234 == 1234 => 4 bulls
 		for (int i = 0; i < compNum.length; i++) {
 			if (guessNum[i]==compNum[i]){
 				bulls++;
--- src/BullsAndCows.java
+++ src/BullsAndCows.java
@@ -22,10 +22,14 @@ public class BullsAndCows {
 //			for (int i : compNumber) {
 //				System.out.print(i);
 //			}
+			//counting Bulls and Cows
 			int cows= Cows.countCows(guessNumber, compNumber);
 			int bulls= Bulls.countBulls(guessNumber, compNumber);
+			//printing how many are bulls and cows
 			System.out.printf("Bulls:%d Cows:%d", bulls, cows);
+			//new line
 			System.out.println();
+			//condition to get out of while
 			if (bulls==4){
 				isGuessed=true;
 				System.out.println("Congratulations");
